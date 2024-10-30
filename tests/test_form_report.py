99dd068810dd9202367a4a2c30c179ca07e84d5c
Mikhail <manufriev927@gmail.com>
2024-10-30 10:31:32 +0200

    Дописал тесты для всех функций

--- tests/test_form_report.py
+++ tests/test_form_report.py
@@ -6,7 +6,7 @@ class TestExstractBasename(unittest.TestCase):
     def tests_exstract(self):
         self.assertEqual(form_report.exstract_basename(
             '/tests/ОТДЕЛ ПРОДАЖ - Сентябрь.csv'),
-                         'ОТДЕЛ ПРОДАЖ - Сентябрь.csv')
+            'ОТДЕЛ ПРОДАЖ - Сентябрь.csv')
         self.assertEqual(form_report.exstract_basename('D:\Work & Projects\Programming_projects'),
                          'Programming_projects')
         self.assertEqual(form_report.exstract_basename(''), '')
@@ -14,9 +14,26 @@ class TestExstractBasename(unittest.TestCase):
 
 class TestNormalizeNumber(unittest.TestCase):
     def tests_comma(self):
-        self.assertEqual(form_report.normalize_number('4.5'), '4.5')
         self.assertEqual(form_report.normalize_number('4,5'), '4.5')
         self.assertEqual(form_report.normalize_number('467,51'), '467.51')
+
+    def test_dot(self):
+        self.assertEqual(form_report.normalize_number('4.5'), '4.5')
+
+    def tests_integer(self):
+        self.assertEqual(form_report.normalize_number('467'), '467')
+        self.assertEqual(form_report.normalize_number('0'), '0')
+
+
+class TestIsCSVFile(unittest.TestCase):
+    def tests(self):
+        self.assertEqual(form_report.is_csv_file(''), False)
+        self.assertEqual(form_report.is_csv_file('D:\Work & Projects\Programming_projects'), False)
+        self.assertEqual(form_report.is_csv_file('ОТДЕЛ ПРОДАЖ - Сентябрь.json'), False)
+        self.assertEqual(form_report.is_csv_file('ОТДЕЛ ПРОДАЖ - Сентябрь.csv'), True)
+        self.assertEqual(form_report.is_csv_file('/tests/ОТДЕЛ ПРОДАЖ - Сентябрь.csv'), True)
+
+
 
 
 if __name__ == '__main__':
