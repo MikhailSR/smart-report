efe6cb897511f5cdad3f2b817905fce3deaedfa9
Mikhail Skliar <manufriev927@gmail.com>
2025-10-19 23:04:30 +0300

    Изменено название файла form_report.py -> form_report_GUI.py
    Имя тестируемого файла в test_form_report.py заменено на созданный псевдоним

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,37 +1,37 @@
 import unittest
-import form_report
+import app as test_file
 
 
 class TestExstractBasename(unittest.TestCase):
     def tests_exstract(self):
-        self.assertEqual(form_report.exstract_basename(
+        self.assertEqual(test_file.exstract_basename(
             '/tests/ОТДЕЛ ПРОДАЖ - Сентябрь.csv'),
             'ОТДЕЛ ПРОДАЖ - Сентябрь.csv')
-        self.assertEqual(form_report.exstract_basename('D:\Work & Projects\Programming_projects'),
+        self.assertEqual(test_file.exstract_basename('D:\Work & Projects\Programming_projects'),
                          'Programming_projects')
-        self.assertEqual(form_report.exstract_basename(''), '')
+        self.assertEqual(test_file.exstract_basename(''), '')
 
 
 class TestNormalizeNumber(unittest.TestCase):
     def tests_comma(self):
-        self.assertEqual(form_report.normalize_number('4,5'), '4.5')
-        self.assertEqual(form_report.normalize_number('467,51'), '467.51')
+        self.assertEqual(test_file.normalize_number('4,5'), '4.5')
+        self.assertEqual(test_file.normalize_number('467,51'), '467.51')
 
     def test_dot(self):
-        self.assertEqual(form_report.normalize_number('4.5'), '4.5')
+        self.assertEqual(test_file.normalize_number('4.5'), '4.5')
 
     def tests_integer(self):
-        self.assertEqual(form_report.normalize_number('467'), '467')
-        self.assertEqual(form_report.normalize_number('0'), '0')
+        self.assertEqual(test_file.normalize_number('467'), '467')
+        self.assertEqual(test_file.normalize_number('0'), '0')
 
 
 class TestIsCSVFile(unittest.TestCase):
     def tests(self):
-        self.assertFalse(form_report.is_csv_file(''))
-        self.assertFalse(form_report.is_csv_file('D:\Work & Projects\Programming_projects'), False)
-        self.assertFalse(form_report.is_csv_file('ОТДЕЛ ПРОДАЖ - Сентябрь.json'), False)
-        self.assertTrue(form_report.is_csv_file('ОТДЕЛ ПРОДАЖ - Сентябрь.csv'), True)
-        self.assertTrue(form_report.is_csv_file('/tests/ОТДЕЛ ПРОДАЖ - Сентябрь.csv'), True)
+        self.assertFalse(test_file.is_csv_file(''))
+        self.assertFalse(test_file.is_csv_file('D:\Work & Projects\Programming_projects'), False)
+        self.assertFalse(test_file.is_csv_file('ОТДЕЛ ПРОДАЖ - Сентябрь.json'), False)
+        self.assertTrue(test_file.is_csv_file('ОТДЕЛ ПРОДАЖ - Сентябрь.csv'), True)
+        self.assertTrue(test_file.is_csv_file('/tests/ОТДЕЛ ПРОДАЖ - Сентябрь.csv'), True)
 
 
 class TestCalculationMetrics(unittest.TestCase):
@@ -154,16 +154,16 @@ class TestCalculationMetrics(unittest.TestCase):
 
 class TestFormatNumberWithSpaces(unittest.TestCase):
     def test_integer(self):
-        self.assertEqual(form_report.format_number_with_spaces(100), "100")
-        self.assertEqual(form_report.format_number_with_spaces(1000), '1 000')
-        self.assertEqual(form_report.format_number_with_spaces(120345), '120 345')
+        self.assertEqual(test_file.format_number_with_spaces(100), "100")
+        self.assertEqual(test_file.format_number_with_spaces(1000), '1 000')
+        self.assertEqual(test_file.format_number_with_spaces(120345), '120 345')
 
     def test_float(self):
-        self.assertEqual(form_report.format_number_with_spaces(100.5), "100,5")
-        self.assertEqual(form_report.format_number_with_spaces(1000.5), "1 000,5")
-        self.assertEqual(form_report.format_number_with_spaces(10000.5), "10 000,5")
-        self.assertEqual(form_report.format_number_with_spaces(100.0), "100")
-        self.assertEqual(form_report.format_number_with_spaces(10000.0), "10 000")
+        self.assertEqual(test_file.format_number_with_spaces(100.5), "100,5")
+        self.assertEqual(test_file.format_number_with_spaces(1000.5), "1 000,5")
+        self.assertEqual(test_file.format_number_with_spaces(10000.5), "10 000,5")
+        self.assertEqual(test_file.format_number_with_spaces(100.0), "100")
+        self.assertEqual(test_file.format_number_with_spaces(10000.0), "10 000")
 
 
 if __name__ == '__main__':
